ce319099ac8b3b6609451514d9c515fb49661181
chrisdeveloper20 <nyaknoandrew75@gmail.com>
2023-05-08 13:18:48 -0700

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and writes its contents to STDOUT.
+ *
+ * @filename: The name of the text file to read.
+ * @letters: The number of characters to read from the file.
+ *
+ * Return: If successful, the actual number of characters read and
+ *            printed to STDOUT.
+ *         If the function fails or @filename is NULL,
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends the given string
+ * to the end of the specified file.
+ *
+ * @filename: A pointer to the name of the file to append to.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails due to a NULL filename
+ * or inability to access the file, return -1.
+ *         Otherwise, return 1 to indicate success.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
